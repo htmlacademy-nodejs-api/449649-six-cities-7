d401adfda68b49f2220a10b96913c614700420c2
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-04-25 15:47:19 +0200

    Зарегистрировал команду ImportCommand.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,3 +2,4 @@ export { CLIApplication } from './cli-application.js';
 export { CommandParser } from './command-parser.js';
 export { HelpCommand } from './commands/help.command.js';
 export { VersionCommand } from './commands/version.command.js';
+export { ImportCommand } from './commands/import.command.js';
--- src/cli/main.cli.ts
+++ src/cli/main.cli.ts
@@ -1,10 +1,12 @@
+import { ImportCommand } from './commands/import.command.js';
 import { CLIApplication, HelpCommand, VersionCommand } from './index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new ImportCommand()
   ]);
 
   cliApplication.processCommand(process.argv);
