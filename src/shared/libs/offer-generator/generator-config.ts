b5ad0c1aa6ae0871f4571a6d01230859065eacf3
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-03 11:49:24 +0200

    Добавил TSVOfferGenerator

---
+++ src/shared/libs/offer-generator/generator-config.ts
@@ -0,0 +1,12 @@
+export const GeneratorConfig = {
+  MinRating: 1,
+  MaxRating: 5,
+  MinBedroom: 1,
+  MaxBedroom: 6,
+  MinRoom: 1,
+  MaxRoom: 6,
+  MinPrice: 10,
+  MaxPrice: 1000,
+  FirstWeekDay: 1,
+  LastWeekDay: 7
+} as const;
---
+++ src/shared/libs/offer-generator/tsv-offer-generator.ts
@@ -0,0 +1,40 @@
+import dayjs from 'dayjs';
+import { GeneratorConfig } from './generator-config.js';
+import { OfferGenerator } from './offer-generator.interface.js';
+import { IMockServerData } from '../../types/index.js';
+import { generateRandomValue, getRandomItem, getRandomItems } from '../../helpers/index.js';
+
+export class TSVOfferGenerator implements OfferGenerator {
+  constructor(private readonly mockData: IMockServerData) { }
+
+  public generate(): string {
+    const title = getRandomItem<string>(this.mockData.titles);
+    const description = getRandomItem<string>(this.mockData.descriptions);
+    const postDate = dayjs()
+      .subtract(generateRandomValue(GeneratorConfig.FirstWeekDay, GeneratorConfig.LastWeekDay), 'day')
+      .toISOString();
+    const city = getRandomItem(this.mockData.cities);
+    const previewImage = getRandomItem(this.mockData.previewImages);
+    const images = getRandomItems(this.mockData.images).join(';');
+    const isPremium = getRandomItem(this.mockData.isPremium);
+    const isFavorite = getRandomItem(this.mockData.isFavorite);
+    const rating = generateRandomValue(GeneratorConfig.MinRating, GeneratorConfig.MaxRating);
+    const type = getRandomItem(this.mockData.types);
+    const room = generateRandomValue(GeneratorConfig.MinRoom, GeneratorConfig.MaxRoom);
+    const bedroom = generateRandomValue(GeneratorConfig.MinBedroom, GeneratorConfig.MaxBedroom);
+    const price = generateRandomValue(GeneratorConfig.MinPrice, GeneratorConfig.MaxPrice);
+    const goods = getRandomItems(this.mockData.goods).join(';');
+    const hostName = getRandomItem(this.mockData.hostNames);
+    const hostEmail = getRandomItem(this.mockData.hostEmails);
+    const hostAvatar = getRandomItem(this.mockData.hostAvatarUrls);
+    const hostPassword = getRandomItem(this.mockData.hostPasswords);
+    const hostType = getRandomItem(this.mockData.hostTypes);
+    const location = getRandomItem(this.mockData.locations);
+
+    return [
+      title, description, postDate, city.name, city.location.latitude, city.location.longitude,
+      previewImage, images, isPremium, isFavorite, rating, type, room, bedroom, price, goods,
+      hostName, hostEmail, hostAvatar, hostPassword, hostType, location.latitude, location.longitude
+    ].join('\t');
+  }
+}
