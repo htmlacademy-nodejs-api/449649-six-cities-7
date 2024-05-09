a81de1649523bb4e005276a8594f347b7274bd71
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-09 16:21:36 +0200

    Добавил класс CreateUserDTO

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,4 +1,2 @@
-export {
-  UserModel,
-  UserEntity,
-} from './user.entity.js';
+export { UserModel, UserEntity } from './user.entity.js';
+export { CreateUserDto } from './dto/create-user.dto.js';
