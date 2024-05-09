1a423fe1b63974598fe71c566f530a3ddddec90d
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-09 12:33:33 +0200

    Добавил привязку для DatabaseClient
    Добавим в контейнер новую привязку для `DatabaseClient`. Свяжем с реализацией `MongoDatabaseClient`

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,6 +5,7 @@ import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { EComponent } from './shared/types/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 
 async function bootstrap() {
@@ -12,7 +13,7 @@ async function bootstrap() {
   container.bind<RestApplication>(EComponent.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(EComponent.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(EComponent.Config).to(RestConfig).inSingletonScope();
-
+  container.bind<DatabaseClient>(EComponent.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
   const application = container.get<RestApplication>(EComponent.RestApplication);
   await application.init();
 }
