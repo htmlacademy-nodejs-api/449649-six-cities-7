f8b05c63052e0e2f5b0d69805aea3f320a0de713
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-09 15:33:03 +0200

    Добавил определение модели и схемы. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,10 +1,23 @@
-import { EUserType } from '../../types/enums.js';
+import { getModelForClass, prop } from '@typegoose/typegoose';
+
 import { IUser } from '../../types/index.js';
+import { EUserType } from '../../types/enums.js';
 
 export class UserEntity implements IUser {
+  @prop({ required: true })
   public name: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarPath: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true })
   public type: EUserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
