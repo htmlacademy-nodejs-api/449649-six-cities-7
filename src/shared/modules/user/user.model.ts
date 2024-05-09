3125892f2b5f8731c6ace4ba27f91c2143c51f38
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-09 13:03:20 +0200

    Расширил интерфейс UserDocument. WIP
    Сначала добавим в интерфейс `UserDocument` два
    поля `createdAt` и `updatedAt` типа `Date`. В первом будем хранить дату создания записи, во втором дату обновления.
    
    Затем при создании схемы передадим объект с настройками. В нём укажем ключ `timestamps` со значением `true`. Тем самым сообщим о
    необходимости учитывать дату создания и обновления записи. Нам не придётся самостоятельно готовить значение для `createdAt` и `updatedAt`.

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -2,7 +2,10 @@ import { Schema, Document, model } from 'mongoose';
 import { IUser } from '../../types/index.js';
 import { EUserType } from '../../types/enums.js';
 
-export interface UserDocument extends IUser, Document {}
+export interface UserDocument extends IUser, Document {
+  createdAt: Date,
+  updatedAt: Date
+}
 
 const userSchema = new Schema({
   name: String,
@@ -10,6 +13,6 @@ const userSchema = new Schema({
   avatarPath: String,
   password: String,
   type: EUserType,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
