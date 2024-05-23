9beb88b90c6b31304ba1a3fe19ce4000c1c01287
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-05-23 10:51:28 +0200

    Расширил интерфейс UserService

---
+++ src/shared/modules/user/dto/update-user.dto.ts
@@ -0,0 +1,9 @@
+import { EUserType } from '../../../types/enums.js';
+
+export class UpdateUserDto {
+  public name?: string;
+  public password?: string;
+  public email?: string;
+  public type?: EUserType;
+  public avatarUrl?: string;
+}
